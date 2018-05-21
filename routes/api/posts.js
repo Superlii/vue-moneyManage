791757edd923bff516e89ca577a2d8e32212df63
Mr.Wu <309595700@qq.com>
2018-05-21 15:41:47 +0800

    add

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -78,4 +78,46 @@ router.delete("/:id",passport.authenticate('jwt', { session: false }),(req,res) 
 })
 
 
+// $route  POST api/posts/like/:id
+// @desc   点赞接口
+// @access Private
+router.post("/like/:id",passport.authenticate('jwt', { session: false }),(req,res) => {
+  Profile.findOne({user:req.user.id}).then(profile => {
+    Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({alreadyliked:"该用户已赞过"})
+          }
+
+          post.likes.unshift({user:req.user.id})
+
+          post.save().then(post => res.json(post))
+        })
+        .catch(err => res.status(404).json({likederror:"点赞错误"}))
+  })
+})
+
+
+// $route  POST api/posts/unlike/:id
+// @desc   取消点赞接口
+// @access Private
+router.post("/unlike/:id",passport.authenticate('jwt', { session: false }),(req,res) => {
+  Profile.findOne({user:req.user.id}).then(profile => {
+    Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({notliked:"该用户没有点过赞"})
+          }
+
+          // 获取要删掉的user id
+          const removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
+
+          post.likes.splice(removeIndex,1);
+
+          post.save().then(post => res.json(post))
+        })
+        .catch(err => res.status(404).json({likederror:"取消点赞错误"}))
+  })
+})
+
 module.exports = router;
